8459793e920417112645a6cef85c45dbd8be14d1
kevinnail <fumalicious@gmail.com>
2023-01-09 15:35:16 -0800

    added sounds on click

--- src/components/Animal/Animal.js
+++ src/components/Animal/Animal.js
@@ -1,9 +1,13 @@
 import './Animal.css';
 
 export default function Animal(props) {
+  let audio = new Audio(`/animals/${props.type}.wav`);
+  const start = () => {
+    audio.play();
+  };
   return (
     <div className="animal" style={{ top: props.top, left: props.left }}>
-      <img src={`/animals/${props.type}.svg`} />
+      <img onClick={start} src={`/animals/${props.type}.svg`} />
       <h1>{props.name}</h1>
       <p>{props.says}</p>
     </div>
