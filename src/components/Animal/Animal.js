0749aaa3fd839cb02af4e40d8e3cd749121a8daf
kevinnail <fumalicious@gmail.com>
2023-01-09 13:53:09 -0800

    text displaying in proper position from animal component

--- src/components/Animal/Animal.js
+++ src/components/Animal/Animal.js
@@ -1,5 +1,10 @@
-import animals from '../../data.js';
+import './Animal.css';
 
 export default function Animal(props) {
-  return <></>;
+  return (
+    <div className="animal" style={{ top: props.top, left: props.left }}>
+      <h1>{props.type}</h1>
+      <h1>{props.says}</h1>
+    </div>
+  );
 }
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -5,5 +5,18 @@ import Animal from '../Animal/Animal.js';
 import background from '../../background.png';
 
 export default function Main() {
-  return <main className="Main" style={{ backgroundImage: `url(${background})` }}></main>;
+  return (
+    <main className="Main" style={{ backgroundImage: `url(${background})` }}>
+      {animals.map((animal) => (
+        <Animal
+          key={animal.name}
+          name={animal.name}
+          type={animal.type}
+          says={animal.says}
+          left={animal.left}
+          top={animal.top}
+        />
+      ))}
+    </main>
+  );
 }
